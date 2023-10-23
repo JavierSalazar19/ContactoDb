8d7459168ecc64914eaa748f23033d2d0b290b9b
cisco01 <cisco01@CISCO1>
2023-10-23 11:59:59 -0700

    Agregar archivos de proyecto.

---
+++ ContactoDb/Controllers/ContactoController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ContactoDb.Datos;
+using ContactoDb.Models;
+using ContactoDb.Datos;
+
+namespace ContactoDb.Controllers
+{
+    public class ContactoController : Controller
+    {
+        ContactoDatos contactosDatos=new ContactoDatos();
+        public IActionResult Listar()
+        {
+            var lista = contactosDatos.Listar();
+            return View(lista);
+        }
+        [HttpGet]
+        public IActionResult Guardar()
+        {
+            return View();  
+        }
+        [HttpPost]
+        public IActionResult Guardar(ContactoModel model)
+        {
+            var respuesta= contactosDatos.GuardarContacto(model);
+            if (respuesta)
+                return RedirectToAction("Listar");
+            else
+            {
+                return View();
+            }
+            return View();
+        }
+    }
+}
---
+++ ContactoDb/Datos/Conexion.cs
@@ -0,0 +1,20 @@
+﻿namespace ContactoDb.Datos
+{
+    public class Conexion
+    {
+        private string cadenaSql = string.Empty;
+
+        public Conexion()
+        {
+            var builder = new ConfigurationBuilder().SetBasePath(
+                Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json").Build();
+                cadenaSql = builder.GetSection("ConnectionString:cadenaSql").Value;
+                
+        }
+
+        public string CadenaSql()
+        {
+            return cadenaSql;
+        }
+    }
+}
